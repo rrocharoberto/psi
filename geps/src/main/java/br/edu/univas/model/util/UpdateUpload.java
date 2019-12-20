144f6fbf7af030d3b76ed17d3ce45affefdcca34
decomartins <andremartinsgti@gmail.com>
2019-12-20 21:29:00 -0200

    Correções na importação dos arquivos

--- geps/src/main/java/br/edu/univas/model/util/UpdateUpload.java
+++ geps/src/main/java/br/edu/univas/model/util/UpdateUpload.java
@@ -2,9 +2,11 @@ package br.edu.univas.model.util;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
@@ -20,7 +22,8 @@ public class UpdateUpload {
 		}
 		String baseDir = args[0];
 
-		Collection<File> listPasta = FileUtils.listFiles(new File(baseDir), FileFilterUtils.trueFileFilter(), null);
+		File [] listPasta = new File(baseDir).listFiles();
+//				FileUtils.listFiles(new File(baseDir), FileFilterUtils.trueFileFilter(), FileFilterUtils.trueFileFilter());
 
 		EntityManager em = HibernateUtil.getEntityManager();
 
@@ -31,34 +34,58 @@ public class UpdateUpload {
 				if (pasta.length > 1) {
 					String numProntuario = pasta[1];
 					
-					Collection<File> listFiles = FileUtils.listFiles(new File(baseDir+ "/" + pasta), FileFilterUtils.trueFileFilter(), null);
+					Collection<File> listFiles = FileUtils.listFiles(new File(f1.getAbsolutePath()), FileFilterUtils.trueFileFilter(), null);
+
+					TypedQuery<Registro> query = em.
+							createQuery(
+									"select r from Registro r where r.numeroProntuario like '%" +numProntuario+
+									"%'", Registro.class);
+					List<Registro> regList = query.getResultList();
 
 					for (File f2 : listFiles) {
-						String[] file = f1.getName().split("_");
-						System.out.println("Processando arquivo: " + f2.getCanonicalPath());
-						
-						String fileName = file[1];
-	
-						em.getTransaction().begin();
-						Registro reg = em.find(Registro.class, numProntuario);
-						if (reg.getDeclaracao().equals(fileName)) {
-							reg.setDeclaracaoContent(FileUtils.readFileToByteArray(f2));
-							em.merge(reg);
-							em.getTransaction().commit();
-							System.out.println("Updated declaracao of registro: " + reg.getNumeroProntuario());
-						} else if (reg.getTermoConsentimento().equals(fileName)) {
-							reg.setTermoContent(FileUtils.readFileToByteArray(f2));
-							em.merge(reg);
-							em.getTransaction().commit();
-							System.out.println("Updated termo of registro: " + reg.getNumeroProntuario());
+						String fullFileName = f2.getName();
+						String [] names = fullFileName.split("_");
+						if(names.length > 1) {
+							String fileName = names[1];
+							System.out.println("Processando arquivo: " + f2.getCanonicalPath());
+							
+							for (Registro reg : regList) {
+								
+								em.getTransaction().begin();
+								if(reg != null) {
+									if (reg.getDeclaracao().equals(fileName)) {
+										reg.setDeclaracaoContent(FileUtils.readFileToByteArray(f2));
+										em.merge(reg);
+										em.getTransaction().commit();
+										System.out.println("Updated declaracao of registro: " + reg.getNumeroProntuario());
+									} else if (reg.getTermoConsentimento().equals(fileName)) {
+										reg.setTermoContent(FileUtils.readFileToByteArray(f2));
+										em.merge(reg);
+										em.getTransaction().commit();
+										System.out.println("Updated termo of registro: " + reg.getNumeroProntuario());
+									} else {
+										System.out.println("Não bateu os nomes: " + reg.getDeclaracao() + " com " + fileName);
+										em.getTransaction().commit();
+									}
+								} else {
+									em.getTransaction().commit();
+									System.out.println("Registro não encontrado para prontuario: " + numProntuario);
+								}
+								//new Scanner(System.in).next();
+							}
+						} else {
+							System.out.println("Parse errado do arquivo: " + fullFileName);
 						}
-						new Scanner(System.in).next();
 					}
 				}
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			System.out.println("Finalizou");
+		}catch(
+
+	Exception e)
+	{
+		e.printStackTrace();
 	}
+}
 
 }
